832623f9826e10ab21b2b6f37afda71e6a5d4f71
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-08-27 09:49:34 -0700

    Create channels and conversations pages

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,52 @@
-const ServerIdPage = () => {
-  return <div>Server ID Page</div>
+import { redirect } from 'next/navigation'
+import { redirectToSignIn } from '@clerk/nextjs'
+
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db'
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string
+  }
+}
+
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile()
+
+  if (!profile) {
+    return redirectToSignIn()
+  }
+
+  // Found the general channel
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: 'general'
+        },
+        orderBy: {
+          createdAt: 'asc'
+        }
+      }
+    }
+  })
+
+  const initialChannel = server?.channels[0]
+
+  if (initialChannel?.name !== 'general') {
+    return null
+  }
+
+  // When entering the server always go to the general channel
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
 }
 
 export default ServerIdPage
